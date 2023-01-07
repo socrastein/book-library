1e2b47fc62d2785a619c064bf4d42d26f25bfeb4
socrastein <31764357+socrastein@users.noreply.github.com>
2023-01-07 13:02:49 -0700

    Add delete book functionality to Library

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+
+
 const BookFunctions = {
-    displayBook() {
+    summary() {
         return `${this.title}, by ${this.author}`
     }
 }
@@ -19,23 +21,47 @@ const BookFactory = (title, author, pages, read) => {
 
 const Library = {
     books: [],
+
     addBook: (book) => Library.books.push(book),
+
+    delBook: (book) => {
+        for(i=0; i<Library.books.length; i++){
+            if(book === Library.books[i]){
+                Library.books.splice(i, 1);
+            }
+        }
+        
+    }
+
 }
 
+// Interacts with the DOM to create new div (card) elements that display
+// on the page
+const CardFactory = (book) => {
 
+    const displayArea = document.getElementById("BookDisplay");
+    const card = document.createElement("div");
+
+    let title = document.createElement("p");
+    title.setAttribute("class", "BookTitle");
+    let author = document.createElement("p");
+    author.setAttribute("class", "BookAuthor");
+    let pages = document.createElement("p");
+    pages.setAttribute("class", "BookPages");
+    let read = document.createElement("input");
+    pages.setAttribute("type", "checkbox");
+    pages.setAttribute("class", "BookRead");
+}
 
 
 let book1 = BookFactory('Book', 'Author', 250, true);
 let book2 = BookFactory('Novel', 'Author', 300, false);
 
 console.log(book1.title);
-console.log(book2.displayBook());
-console.log(book1.displayBook === book2.displayBook)
+console.log(book2.summary());
 
 Library.addBook(book1);
 Library.addBook(book2);
 console.log(Library.books);
 
 Library.books.forEach(book => console.log(book.title, book.author));
-
-BookDisplay.displayAll(Library.books);
